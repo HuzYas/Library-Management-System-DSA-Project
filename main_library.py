e51e10e6a24cf0329cb4d0b7bc9789bc45dfc05b
Huzaifah Yasin <112056717+HuzYas@users.noreply.github.com>
2024-04-18 12:21:32 +0500

    k

--- main_library.py
+++ main_library.py
@@ -2,4 +2,15 @@ from HelperFunctions import *
 from Library_data import *
 
 
+tree = create_node(book_dict['The Lost Chronicles'])
+print(tree)
 
+
+for i in book_dict:
+  if i == "The Lost Chronicles":
+    continue
+  else: 
+    insert(tree, i)
+
+    
+print(tree)
